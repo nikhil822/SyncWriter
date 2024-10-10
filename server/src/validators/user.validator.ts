85f26fe09e2932561ad745f7a0164d5948739a76
nikhil822 <nikhilsahu1312@gmail.com>
2024-10-11 00:18:24 +0530

    added more functionality in  user validator

--- server/src/validators/user.validator.ts
+++ server/src/validators/user.validator.ts
@@ -2,19 +2,33 @@ import { body } from "express-validator";
 import { userService } from "../services/user.service";
 
 const registerValidator = [
-    body("email").isEmail().normalizeEmail().withMessage("Must provide email address"),
-    body('email').custom(async (value) => {
-        const user = await userService.findUserByEmail(value)
-        if(user) {
-            return Promise.reject("User with this email already exists.")
-        }
-        return true
-    }),
-
-]
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Must provide email address"),
+  body("email").custom(async (value) => {
+    const user = await userService.findUserByEmail(value);
+    if (user) {
+      return Promise.reject("User with this email already exists.");
+    }
+    return true;
+  }),
+  body("password1")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters"),
+  body("password1")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one digit."),
+  body("password2").custom((value, { req }) => {
+    if (value !== req.body.password1) {
+      throw new Error("Passwords must match.");
+    }
+    return true;
+  }),
+];
 
 export const userValidator = {
   register: registerValidator,
-//   resetPassword: resetPasswordValidator,
-//   confirmResetPassword: confirmResetPasswordValidator,
+  //   resetPassword: resetPasswordValidator,
+  //   confirmResetPassword: confirmResetPasswordValidator,
 };
